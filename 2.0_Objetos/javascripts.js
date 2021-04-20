45f414c0876c7f1f2dcee882ef53ab7dc1e5c5ec
Jaime Llanos <jaimeantonioadd@gmail.com>
2021-04-20 02:23:30 -0400

    Objetos

---
+++ 2.0_Objetos/javascripts.js
@@ -0,0 +1,42 @@
+
+
+// const persona = {
+//     nombre:'marco',
+//     apellido:'Gaytan',
+//     edad : 22,
+//     sexo : ['masculino','femenino'],
+// }
+
+
+// const auto = {
+//     color:'negro',
+//     marca : ['nissan','toyota','hinduay','Kia'],
+//     mostrarDatos : function(){
+//         return `${this.color} ${this.marca}`
+//     },
+//     precio : {
+//         Basico :500.000,
+//         medio : 1000000,
+//         alto: 10000000
+//     },
+//     pagado : true,
+//     mensaje :'',
+//     dato : null 
+// }
+
+// // //Como accedemos a los datos que estan dentro del objeto
+// // auto.mensaje;
+// // auto['marca'];
+
+// // let keys = Object.keys(auto);
+
+// // keys.forEach((elemento,indice)=>{
+// //     console.log(`#${indice} ${elemento}`)
+// // })
+
+// auto.color = 'Blanco';
+// auto.mascota = 'perro'
+// console.log(auto);
+// console.log(Object.values(auto));
+// console.log(auto.hasOwnProperty('precio'));
+// console.log(auto.hasOwnProperty('poto'));
---
+++ 2.0_Objetos/teoria.md
@@ -0,0 +1,11 @@
+# Objetos.
+* Un objeto almacena diferente tipos de datos.(Datos Primitivos, Datos Compuestos.)
+*Sintaxis* const nombreObjeto ={ }
+* Como acceder a un objeto : con el punto, con el Nombre de la propiedad del objeto.
+* Tambien son  llamados atributos o propiedades.
+* Las funciones en los objetos son denominados Metodos.
+* Ordena las propiedades en orden alfabeticos.
+## Metodos.
+**Keys** : El metodo key, te permite obtener todas las llave del objeto.
+**Values** : El metodo value , te entrega el valor de la propiedad del objeto.
+**hasOwnProperty** : validad si la propiedad existe.

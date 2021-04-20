04ee688e24545159b0e8ef7397283289e5f49f26
Jaime Llanos <jaimeantonioadd@gmail.com>
2021-04-20 03:25:14 -0400

    tipos de operadores

---
+++ 2.1_Tipo_De_Operadores/javascripts.js
@@ -0,0 +1,35 @@
+
+// let nu1,nu2,n3,n4;
+// nu1=20;
+// nu2=30;
+// n3=10;
+// n4= 5;
+// let resultado = nu1 + nu2 - (n4-nu2)
+// console.log(resultado);
+
+// console.log(2>5)
+// console.log(210>325)
+// console.log(22>=22)
+// console.log('jaime'=='juan')
+// console.log('jaime'==='juan')
+// console.log('5'== 5)
+// console.log('5'=== 5)
+
+// let num1 = '3';
+// let num2 = '30';
+
+// console.log(num1*num2)
+
+
+
+// let numero = 0;
+// numero+= 1;
+// numero -= 1;
+// numero *= 1;
+// numero /= 1;
+// numero++
+// numero--
+// console.log(numero+=1)
+
+// console.log((3>5)||(5===5))
+// console.log((5>=5)&&(5===5))
---
+++ 2.1_Tipo_De_Operadores/teoria.md
@@ -0,0 +1,8 @@
+# Tipo de operadores :)
+**Operadores Aritemticos**: +,-,*,/,%
+**Operadores Relacionales** : >,<,>=,<=, =,==,===,!=,!==
+* Los operadores relaciones === : especifica su valor,tipo de dato.
+* Los operadores relaciones == : verifican su valor, Ya no es buena practica utilizar.
+* operadores de incremento e Decrementos : Su funcionalidad es incrementar y decrementar un valor.
+* operadore unario.
+* operadores Logicos : and(&&), or(||) ,Not(!);

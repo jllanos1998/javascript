bc1ff47a7c7729135a7dd5e988d99556351b0e22
Jaime Llanos <jaimeantonioadd@gmail.com>
2021-05-25 19:59:10 -0400

    asincronia

--- 3.4_Programacion_asincrona/javascripts.js
+++ 3.4_Programacion_asincrona/javascripts.js
@@ -1,5 +1,3 @@
-
-
 // const usuarios=[
 //     {id:1 , nombre:'jaime', profesion_id: 1},
 //     {id:2 , nombre:'Angel', profesion_id: 1},
@@ -16,7 +14,6 @@
 //         callback(null,usuarios)
 //     },200)
 // }
-
 
 // function getUsuario(id, callback){
 //     setTimeout(()=>{
@@ -39,14 +36,11 @@
 //     })
 // })
 
-
-
 // function cuadradoCallback(value,callback){
 //     setTimeout(()=>{
 //         callback(value,value*value);
 //     },Math.random()*100)
 
-    
 // }
 
 // cuadradoCallback(5,(value,resultado)=>{
@@ -59,64 +53,123 @@
 //                 console.log(`value: ${value} resultado:${resultado}`)
 //                 cuadradoCallback(25,(value,resultado)=>{
 //                     console.log(`value: ${value} resultado:${resultado}`)
-                    
+
 //                 })
-                
+
 //             })
-            
+
 //         })
-        
+
 //     })
 
 // })
 
-const personas = [
-    {nombre:'jaime', edad:30,sexo:'Masculino'},
-    {nombre:'angel', edad:40,sexo:'Masculino'},
-    {nombre:'javiera', edad:20,sexo:'femenino'},
-    {nombre:'esteban', edad:13,sexo:'Masculino'},
-    {nombre:'roberto', edad:30,sexo:'Masculino'},
-]
+// const personas = [
+//     {nombre:'jaime', edad:30,sexo:'Masculino'},
+//     {nombre:'angel', edad:40,sexo:'Masculino'},
+//     {nombre:'javiera', edad:20,sexo:'femenino'},
+//     {nombre:'esteban', edad:13,sexo:'Masculino'},
+//     {nombre:'roberto', edad:30,sexo:'Masculino'},
+// ]
 
-const contenedor = document.getElementById('contenedor');
-const button = document.getElementById('button');
-const body = document.getElementById('body')
-function getPersona(){
-    let nombre;
-    let edad;
-    let sexo;
-    let div = document.createElement('div');
-    load()
-    personas.forEach((valor)=>{
-        setTimeout(()=>{
-            nombre = valor['nombre'];
-            edad = valor['edad']
-            sexo = valor['sexo'];
-            div.innerHTML += `<div class=>
-            <h2>${nombre}</h2>
-            <p>${edad}</p>
-            <p>${sexo}</p>  
-            </div>`
-            
-        },10000)
-       
-        contenedor.appendChild(div)
-      })
-    
-   
-}
-function load(){
-    let spinner = document.createElement('div');
-    spinner.classList.add('spinner')
-    body.appendChild(spinner);
-    setTimeout(()=>{
-        spinner.remove()
-    },9000)
-}
+// const contenedor = document.getElementById('contenedor');
+// const button = document.getElementById('button');
+// const body = document.getElementById('body')
+// function getPersona(){
+//     let nombre;
+//     let edad;
+//     let sexo;
+//     let div = document.createElement('div');
+//     load()
+//     personas.forEach((valor)=>{
+//         setTimeout(()=>{
+//             nombre = valor['nombre'];
+//             edad = valor['edad']
+//             sexo = valor['sexo'];
+//             div.innerHTML += `<div class=>
+//             <h2>${nombre}</h2>
+//             <p>${edad}</p>
+//             <p>${sexo}</p>
+//             </div>`
 
-  
-button.addEventListener('click',()=>{
-    getPersona();
-})
+//         },10000)
+
+//         contenedor.appendChild(div)
+//       })
+
+// }
+// function load(){
+//     let spinner = document.createElement('div');
+//     spinner.classList.add('spinner')
+//     body.appendChild(spinner);
+//     setTimeout(()=>{
+//         spinner.remove()
+//     },9000)
+// }
+
+// button.addEventListener('click',()=>{
+//     getPersona();
+// })
+
+// function cuadradoPromesa(valor){
+//     if(typeof valor !== 'number'){
+//         return Promise.reject(`El valor ingresado es de tipo ${typeof valor} y debes ingresar numeros pinche pendejo`)
+//     }
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(
+//                 {
+//                     valor,
+//                     resultado:valor*valor
+//                 }
+//             )
+//             reject('No haz ingresado un valor numerico')
+//         },1000)
+//     })
+// }
+
+// cuadradoPromesa(1).then((dato)=>{
+//     console.log(dato)
+//    return cuadradoPromesa(2)
+// }).then((dato)=>{
+//     console.log(dato)
+//     return cuadradoPromesa(3)
+// }).then((dato)=>{
+//     console.log(dato)
+//     return cuadradoPromesa(4)
+// }).then((dato)=>{
+//     console.log(dato)
+//     return cuadradoPromesa(5)
+// }).then((dato)=>{
+//     console.log(dato)
+// })
+// .catch((err)=>{
+//     console.error(err)
+// })
 
 
+
+// function treardatosApi() {
+//   let $tr= document.getElementById('tr');
+
+ 
+//   return new Promise((res, rej) => {
+//     res(
+//       fetch("https://jsonplaceholder.typicode.com/posts/1/comments").then(
+//         (datos) => {
+//           datos.json().then((datos) => {
+//             for (let i in datos) {
+//               let $td = document.createElement('td')
+//               $td.textContent = datos[i]["email"]
+//               $tr.appendChild($td)
+//             }
+
+//            ;
+//           });
+//         }
+//       )
+//     );
+//   });
+// }
+
+// treardatosApi();
